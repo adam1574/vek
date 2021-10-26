6ac7050fa5cf2dc8d58e4fd30d57331bac4217e3
Sivera.Adam <sivera.adam@seznam.cz>
2021-10-26 10:27:47 +0200

    Add project files.

---
+++ vek/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace vek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("výpočet věku");
+            Console.WriteLine("_______________");
+
+        }
+    }
+}
